4198e4c3065d947b4f966b804c1f876f5634f254
danilopiccinni <danilopiccinni@gmail.com>
2023-03-23 14:06:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+  import Titolo from "./components/AppTitle.vue";
+
+  export default {
+
+    components: {
+      Titolo,
+    }
+  } 
+
+</script>
+
+<template>
+
+  <Titolo></Titolo>
+  
+</template>
+
+<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+
+    export default {
+        data() {
+            return {
+                title: "La mia prima app con Vite!",
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+    <h1> {{ title }} </h1>
+
+</template>
+
+<style>
+
+    h1 {
+        color: red;
+    }
+
+</style>
